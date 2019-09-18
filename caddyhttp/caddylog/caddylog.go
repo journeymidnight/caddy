5798dc1ff0fd565099cb8c371fe7b62bb2e2b0d3
fireworkmarks <806229693@qq.com>
2019-09-19 04:01:25 +0800

    WIP Sometimes unable to generate a logger

--- caddyhttp/caddylog/caddylog.go
+++ caddyhttp/caddylog/caddylog.go
@@ -6,7 +6,6 @@ import (
 	"github.com/journeymidnight/yig-front-caddy/caddylog"
 	"github.com/journeymidnight/yig-front-caddy/helper"
 	"net/http"
-	"os"
 )
 
 var logger *caddylog.Logger
@@ -18,13 +17,6 @@ type Log struct {
 }
 
 func (l Log) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
-	f, err := os.OpenFile(l.LogPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		panic("Failed to open log file " + l.LogPath)
-	}
-
-	logger = caddylog.New(f, "[yig-front-caddy]", caddylog.LstdFlags, l.LogLevel)
-	helper.Logger = logger
 	ctx := context.WithValue(r.Context(), "logger", &helper.Log{Logger: logger})
 	return l.Next.ServeHTTP(w, r.WithContext(ctx))
 }
