8088695f2d583fa15bc51a1273234be7eefa9662
fireworkmarks <806229693@qq.com>
2019-09-23 22:09:07 +0800

    WIP Modify the database file

--- conf/caddy.sql
+++ conf/caddy.sql
@@ -1,20 +1,20 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 10.0.47.136
+ Source Server         : 10.5.0.17
  Source Server Type    : MySQL
  Source Server Version : 50725
- Source Host           : 10.0.47.136:4000
- Source Schema         : unis
+ Source Host           : 10.5.0.17:4000
+ Source Schema         : caddy
 
  Target Server Type    : MySQL
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 12/09/2019 11:26:33
+ Date: 22/09/2019 21:47:17
 */
 
-SET NAMES utf8mb4;
+SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
@@ -25,7 +25,7 @@ CREATE TABLE `custom_domain`  (
   `project_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `host_domain` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `bucket_domain` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  INDEX `domain`(`project_id`, `host_domain`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 100 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+  PRIMARY KEY (`project_id`, `host_domain`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;
