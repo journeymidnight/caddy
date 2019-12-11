65ef30c908a7245a8fc5c644ac6b5f7d36572d90
fireworkmarks <806229693@qq.com>
2019-12-12 05:47:26 +0800

    Added some verifications and made some modifications to the original API

--- caddyhttp/pipa/setup.go
+++ caddyhttp/pipa/setup.go
@@ -2,6 +2,7 @@ package pipa
 
 import (
 	caddy "github.com/journeymidnight/yig-front-caddy"
+	"github.com/journeymidnight/yig-front-caddy/caddyhttp/clients/clients/tidbclient"
 	"github.com/journeymidnight/yig-front-caddy/caddyhttp/httpserver"
 	"strconv"
 )
@@ -15,17 +16,22 @@ func init() {
 
 // setup configures a new mime middleware instance.
 func setup(c *caddy.Controller) error {
-	redisMaxIdle, redisAddress, redisPwd, secretKey, err := pipaParse(c)
+	redisMaxIdle, redisAddress, redisPwd, secretKey, s3Source, caddySource, db, err := pipaParse(c)
 	if err != nil {
 		return err
 	}
 	httpserver.GetConfig(c).AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
-		return Pipa{Next: next, redis: newRedisPool(redisMaxIdle, redisAddress, redisPwd), SecretKey: secretKey}
+		return Pipa{
+			Next:      next,
+			redis:     newRedisPool(redisMaxIdle, redisAddress, redisPwd),
+			Client:    tidbclient.NewCustomDomainClient(s3Source, caddySource, db),
+			SecretKey: secretKey,
+		}
 	})
 	return nil
 }
 
-func pipaParse(c *caddy.Controller) (redisMaxIdle int, redisAddress, redisPwd, secretKey string, err error) {
+func pipaParse(c *caddy.Controller) (redisMaxIdle int, redisAddress, redisPwd, secretKey, s3Source, caddySource string, db tidbclient.DBInfo, err error) {
 	for c.Next() {
 		for c.NextBlock() {
 			ext := c.Val()
@@ -61,6 +67,50 @@ func pipaParse(c *caddy.Controller) (redisMaxIdle int, redisAddress, redisPwd, s
 					return
 				}
 				secretKey = c.Val()
+			case "s3_db":
+				if !c.NextArg() {
+					err = c.ArgErr()
+					return
+				}
+				s3Source = c.Val()
+				break
+			case "caddy_db":
+				if !c.NextArg() {
+					err = c.ArgErr()
+					return
+				}
+				caddySource = c.Val()
+				break
+			case "db_max_idle_conns":
+				if !c.NextArg() {
+					err = c.ArgErr()
+					return
+				}
+				int, err := strconv.Atoi(c.Val())
+				if err != nil {
+					return -1, "", "", "", "", "", db, err
+				}
+				db.DBMaxIdleConns = int
+			case "db_max_open_conns":
+				if !c.NextArg() {
+					err = c.ArgErr()
+					return
+				}
+				int, err := strconv.Atoi(c.Val())
+				if err != nil {
+					return -1, "", "", "", "", "", db, err
+				}
+				db.DBMaxOpenConns = int
+			case "db_conn_max_life_seconds":
+				if !c.NextArg() {
+					err = c.ArgErr()
+					return
+				}
+				int, err := strconv.Atoi(c.Val())
+				if err != nil {
+					return -1, "", "", "", "", "", db, err
+				}
+				db.DBConnMaxLifeSeconds = int
 			}
 		}
 	}
