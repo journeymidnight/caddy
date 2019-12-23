4eaf85bee3b598e6006b99e3488d5a409f7e98ca
fireworkmarks <806229693@qq.com>
2019-12-24 00:08:22 +0800

    Fix database table

--- conf/caddy.sql
+++ conf/caddy.sql
@@ -4,14 +4,14 @@
  Source Server         : 10.0.47.136
  Source Server Type    : MySQL
  Source Server Version : 50725
- Source Host           : 10.0.42.26:4000
+ Source Host           : 10.0.47.136:4000
  Source Schema         : caddy
 
  Target Server Type    : MySQL
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 11/12/2019 17:42:22
+ Date: 26/09/2019 16:30:02
 */
 
 SET NAMES utf8;
@@ -25,10 +25,10 @@ CREATE TABLE `custom_domain`  (
   `project_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `host_domain` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `bucket_domain` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `tls_domain` blob DEFAULT NULL,
-  `tls_domain_key` blob DEFAULT NULL,
+  `tls_domain` BLOB DEFAULT NULL,
+  `tls_domain_key` BLOB DEFAULT NULL,
   PRIMARY KEY (`project_id`, `host_domain`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
 
 -- ----------------------------
 -- Table structure for images_style
@@ -39,7 +39,6 @@ CREATE TABLE `images_style`  (
   `style_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `style_code` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`bucket_name`, `style_name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;
-
